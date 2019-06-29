2423f5ab9ddfee862b76cdb9b606e3b0a018ed86
segalll <hseg1227@gmail.com>
2019-06-28 20:47:05 -0500

    Nullcheck playlist

--- DiscordRPCPlugin/DiscordRPCPlugin.cpp
+++ DiscordRPCPlugin/DiscordRPCPlugin.cpp
@@ -114,7 +114,11 @@ Data DiscordRPCPlugin::HandleOnlineGame(Data input, GameState gameState) {
 	Data output = input;
 	
 	GameSettingPlaylistWrapper playlistWrapper = gameState.wrapper.GetPlaylist();
-	string gameType = GetPlaylistName(playlistWrapper.GetPlaylistId(), gameState.wrapper.GetMaxTeamSize());
+
+	string gameType;
+	if (playlistWrapper.memory_address != NULL) {
+		gameType = GetPlaylistName(playlistWrapper.GetPlaylistId(), gameState.wrapper.GetMaxTeamSize());
+	}
 	
 	int teamGoals[2] = { 0, 0 };
 	if (!gameState.wrapper.GetTeams().Get(0).IsNull() && !gameState.wrapper.GetTeams().Get(1).IsNull()) {
